5cafd0a632353a264a423aef8136f22ee6ad53ef
Nouvy <95512507+Nouvy@users.noreply.github.com>
2025-03-25 15:26:02 +0100

    Correction faille command injection

--- command.php
+++ command.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($_GET['ip'])) {
-    $ip = $_GET['ip'];
+    //$ip = $_GET['ip'];
+    $ip = escapeshellarg($_GET['ip']);
     $commande =  shell_exec("ping -c 1 " . $ip);
     echo "<pre>" . $commande . "</pre>";
 } else {
